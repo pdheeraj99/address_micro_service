0d1084e5b25dfcfc6f3b86b82a1854c81af0b7f5
dheeraj <pilladheeraj@gmail.com>
2023-11-16 18:57:31 +0530

    updated spring boot properties

--- src/main/java/com/springlearn/address_service/AddressServiceApplication.java
+++ src/main/java/com/springlearn/address_service/AddressServiceApplication.java
@@ -2,8 +2,19 @@ package com.springlearn.address_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({ "com.springlearn.controller", "com.springlearn.service" })
+@EntityScan("com.springlearn.entity")
+@EnableJpaRepositories("com.springlearn.repository")
+/**
+ * The AddressServiceApplication class is the entry point for running the
+ * address service application
+ * in Java.
+ */
 public class AddressServiceApplication {
 
 	public static void main(String[] args) {
